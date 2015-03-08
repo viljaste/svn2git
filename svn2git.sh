6588a51fca6063610b056ebbce44594785ecd16a
Jürgen Viljaste <j.viljaste@gmail.com>
2015-03-08 18:29:54 +0200

    Update

--- svn2git.sh
+++ svn2git.sh
@@ -10,7 +10,7 @@ if [ "${?}" -ne 0 ]; then
   exit 1
 fi
 
-git svn --version 2> /dev/null
+git svn --version > /dev/null 2>&1
 
 if [ "${?}" -ne 0 ]; then
   echo "svn2git: git svn command not found."
